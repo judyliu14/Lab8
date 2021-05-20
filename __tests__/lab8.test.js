8cca76037a174f37c897e98d48d1d98bc023cb9a
LexuanLiu123 <lel006@ucsd.edu>
2021-05-19 21:44:39 -0700

    first commit

--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -1,3 +1,5 @@
+import { expect } from "@jest/globals";
+
 describe('Basic user flow for SPA ', () => {
   beforeAll(async () => {
     await page.goto('http://127.0.0.1:5500');
@@ -29,12 +31,17 @@ describe('Basic user flow for SPA ', () => {
 
   it('Test3: Clicking first <journal-entry>, new URL should contain /#entry1', async () => {
     // implement test3: Clicking on the first journal entry should update the URL to contain “/#entry1”
-
+    await page.click("journal-entry");
+    const url = await page.url();
+    expect(url).toBe("http://127.0.0.1:5500/#entry1");
   });
 
   it('Test4: On first Entry page - checking page header title', async () => {
     // implement test4: Clicking on the first journal entry should update the header text to “Entry 1” 
-
+    const header = await page.$eval("h1", (entries) => {
+      return entries.innerHTML;
+    });
+    expect(header).toBe("Entry 1");
   });
 
   it('Test5: On first Entry page - checking <entry-page> contents', async () => {
@@ -50,6 +57,17 @@ describe('Basic user flow for SPA ', () => {
           }
         }
       */
+    const actual_entry_title = await page.$("entry-title");
+    const expected_entry = {
+      title: 'You like jazz?',
+      date: '4/25/2021',
+      content: "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.",
+      image: {
+        src: 'https://i1.wp.com/www.thepopcornmuncher.com/wp-content/uploads/2016/11/bee-movie.jpg?resize=800%2C455',
+        alt: 'bee with sunglasses'
+      }
+     };
+     expect(actual_entry_title).toBe(expected_entry.title);
 
   }, 10000);
 
--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -2,3 +2,16 @@
  * @jest-environment jsdom
  */
 
+import { pushToHistory } from '../scripts/router.js';
+
+
+describe("test pushToHistory", () => {
+    test("tests length of history", () => {
+        expect(pushToHistory("entry", 5).length).toBe(2);
+        expect(pushToHistory("entry", 5).length).toBe(3);
+    });
+    test("tests current state object", () => {
+        expect(pushToHistory("settings", 5).state).toStrictEqual({"page": "settings"});
+        expect(pushToHistory("entry", 4).state).toStrictEqual({"page": "entry4"})
+    })
+});
